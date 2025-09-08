532c49945f017f4bc9e18094aad30c7de5237fdb
LongYinan <lynweklm@gmail.com>
2025-09-08 11:26:31 +0800

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# AARCH64 `jsconv` benchmark
+
+This is a benchmark for the [`jsconv`](https://developer.arm.com/documentation/ddi0602/2022-03/SIMD-FP-Instructions/FJCVTZS--Floating-point-Javascript-Convert-to-Signed-fixed-point--rounding-toward-Zero-) instruction on AARCH64.
---
+++ benches/jsconv.rs
@@ -0,0 +1,40 @@
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+
+use jsconv_aarch64::{f64_to_int32_arm64, f64_to_int32_generic};
+
+fn bench_jsconv(c: &mut Criterion) {
+    let fixtures = [
+        0.0,
+        -0.0,
+        1.0,
+        -1.0,
+        42.7,
+        -42.7,
+        f64::from(i32::MAX),
+        f64::from(i32::MIN),
+        f64::from(i32::MAX) + 1.0,
+        f64::from(i32::MIN) - 1.0,
+        9_007_199_254_740_992.0,  // 2^53
+        -9_007_199_254_740_992.0, // -2^53
+    ];
+
+    c.bench_function("jsconv_with_atomics", |b| {
+        b.iter(|| {
+            for fixture in fixtures {
+                black_box(unsafe { f64_to_int32_arm64(fixture) });
+            }
+        });
+    });
+    c.bench_function("generic", |b| {
+        b.iter(|| {
+            for fixture in fixtures {
+                black_box(f64_to_int32_generic(fixture));
+            }
+        });
+    });
+}
+
+criterion_group!(benches, bench_jsconv);
+criterion_main!(benches);
